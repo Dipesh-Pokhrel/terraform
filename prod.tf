c10f32d5bd83f868caa839c839eeb235f1b1f4e1
Dipesh Pokhrel <dipeshpokhrel45@gmail.com>
2022-08-08 14:44:13 +0545

    load balancer for prod web

--- prod.tf
+++ prod.tf
@@ -11,6 +11,22 @@ resource "aws_s3_bucket" "prod_tf_dipesh" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "ap-south-1b"
+  
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound"
@@ -56,7 +72,7 @@ resource "aws_instance" "prod_web" {
 }
 
 resource "aws_eip_association" "prod_web" {
-  instance_id  = aws_instance.prod_web.0.id
+  instance_id   = aws_instance.prod_web.0.id
   allocation_id = aws_eip.prod_web.id
 }
 
@@ -65,3 +81,18 @@ resource "aws_eip" "prod_web" {
    "Terraform" : "true"
   }
 }
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+  
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
